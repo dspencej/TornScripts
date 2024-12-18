ec58e433a3b9f7aa0b2b1124c03170c3d80421c3
Dustin Spencer <110250829+dspencej@users.noreply.github.com>
2024-12-18 18:57:00 -0500

    Update hospitalFilter.js

--- scripts/hospitalFilter.js
+++ scripts/hospitalFilter.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Torn Hospital Revive Filter (Observer Enhanced)
+// @name         Torn Hospital Revive Filter
 // @namespace    https://github.com/dspencej/TornScripts
 // @version      2.9.0
 // @description  Adds filtering functionality to the Torn hospital page with persistent filters, and ensures filters are applied once the user list is available.
