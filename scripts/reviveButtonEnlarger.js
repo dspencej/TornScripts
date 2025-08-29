6d024ae38d7138087f391f60f6825df2e6c1c86d
spencdj1 <dustin.spencer@jhuapl.edu>
2025-08-29 15:14:14 -0400

    reviveButtonEnlarger: fix userscript headers; double revive button and icon sizes

--- scripts/reviveButtonEnlarger.js
+++ scripts/reviveButtonEnlarger.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name         Torn Revive Button Enbiggerator
+// @name         Torn Revive Button Enlarger
 // @namespace    https://github.com/dspencej/TornScripts
-// @version      1.2.0
+// @version      1.3.0
 // @description  Enlarges the revive button and hides the display case button on a user's profile page in Torn.
 // @author       dspencej
 // @license      MIT
 // @match        https://www.torn.com/profiles.php?XID=*
-// @downloadURL  https://raw.githubusercontent.com/dspencej/TornScripts/refs/heads/main/scripts/profileButtonEnhancements.js
-// @updateURL    https://raw.githubusercontent.com/dspencej/TornScripts/refs/heads/main/scripts/profileButtonEnhancements.js
+// @downloadURL  https://raw.githubusercontent.com/dspencej/TornScripts/main/scripts/reviveButtonEnlarger.js
+// @updateURL    https://raw.githubusercontent.com/dspencej/TornScripts/main/scripts/reviveButtonEnlarger.js
+// @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function () {
@@ -18,16 +20,16 @@
         const reviveButton = document.querySelector('.profile-button-revive');
         if (reviveButton) {
             // Enlarge the button
-            reviveButton.style.width = '100px';
-            reviveButton.style.height = '100px';
+            reviveButton.style.width = '200px';
+            reviveButton.style.height = '200px';
             reviveButton.style.padding = '10px';
             reviveButton.style.margin = '10px';
 
             // Enlarge the SVG icon within the button
             const svgIcon = reviveButton.querySelector('svg');
             if (svgIcon) {
-                svgIcon.style.width = '80px';
-                svgIcon.style.height = '80px';
+                svgIcon.style.width = '160px';
+                svgIcon.style.height = '160px';
             }
 
             console.log('Revive button has been enlarged.');
