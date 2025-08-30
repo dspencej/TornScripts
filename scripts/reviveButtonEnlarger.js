6dc529ca6b2f763e00b2acf74c883f5dad42710e
Dustin Spencer <dspencej@gmail.com>
2025-08-29 23:43:49 -0400

    updated

--- scripts/reviveButtonEnlarger.js
+++ scripts/reviveButtonEnlarger.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Revive Button Enlarger
 // @namespace    https://github.com/dspencej/TornScripts
-// @version      1.9.0
+// @version      1.10.0
 // @description  Enlarges the revive button, hides display case button, and centers
 // @author       dspencej
 // @license      MIT
@@ -131,8 +131,6 @@
                             document.removeEventListener('keydown', _0x16);
                         };
                         document.addEventListener('keydown', _0x16);
-                    } else {
-                        _0x10(noButton);
                     }
                 }
             }
